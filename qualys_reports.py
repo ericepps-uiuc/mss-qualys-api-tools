c6048eec0a54a1b02fd2c03f1c4149895be41045
ericepps-uiuc <eepps2@illinois.edu>
2024-07-31 20:05:05 -0500

    add box upload
    upload file to Box folder

--- qualys_reports.py
+++ qualys_reports.py
@@ -5,7 +5,15 @@ import json
 import jmespath
 from requests.auth import HTTPBasicAuth
 import xml.etree.ElementTree as ET
+from box_sdk_gen import (
+    UploadFileAttributes,
+   	UploadFileAttributesParentField,
+    Files,
+   	File,
+)
+from box_sdk_gen import BoxClient, BoxDeveloperTokenAuth
 
+#get qualys credentials from vault
 vault_url = 'https://smg-vault.techservices.illinois.edu:8200/v1/wsaa/apiaccess/qualys'
 vault_token = os.environ["VAULT_TOKEN"]
 headers = {'X-Vault-Token': vault_token}
@@ -15,6 +23,21 @@ vault_response_json = vault_response.json()
 
 qualys_un = json.dumps(vault_response_json["data"]["user"]).strip('\"')
 qualys_pw = json.dumps(vault_response_json["data"]["password"]).strip('\"')
+
+#get box credentials from vault
+vault_url = 'https://smg-vault.techservices.illinois.edu:8200/v1/wsaa/apiaccess/Box'
+vault_token = os.environ["VAULT_TOKEN"]
+headers = {'X-Vault-Token': vault_token}
+
+vault_response = requests.get(vault_url, headers=headers)
+vault_response_json = vault_response.json()
+
+box_token = json.dumps(vault_response_json["data"]["dev-token"]).strip('\"')
+#box_id = json.dumps(vault_response_json["data"]["client-id"]).strip('\"')
+#box_secret = json.dumps(vault_response_json["data"]["client-secret"]).strip('\"')
+
+auth: BoxDeveloperTokenAuth = BoxDeveloperTokenAuth(token=box_token)
+client: BoxClient = BoxClient(auth=auth)
 
 def qualys_call(qualys_endpoint, qualys_action):
 	global qualys_un
@@ -52,6 +75,17 @@ for elem in qualys_reponse_xml.findall('.//REPORT'):
 	qualys_report_action = '?action=fetch&id=' + report_id
 	qualys_report_response = qualys_call(qualys_endpoint, qualys_report_action)
 	
-	with open(report_title + '_' + report_time + '.' + report_format, "wb") as report:
+	report_file_name = report_title + '_' + report_time + '.' + report_format
+	with open(report_file_name, "wb") as report:
 		report.write(qualys_report_response.content)
+
+	attrs = UploadFileAttributes(
+    	name=report_file_name, parent=UploadFileAttributesParentField(id="277885701226")
+	)
+	files: Files = client.uploads.upload_file(
+    	attributes=attrs, file=open(report_file_name, "rb")
+	)
+	file: File = files.entries[0]
+	print(f"File uploaded with id {file.id}, name {file.name}")
+
 
